16a76e8ec8af2d0267a3944c9676116519c024f5
intiqamquluzada <quluzadintiqam@gmail.com>
2023-11-12 22:15:12 +0400

    jazzmin using

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,9 @@ if not DEBUG:
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+
 MEDIA_URL = "/media/"
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
